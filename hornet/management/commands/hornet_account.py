77d4b96e1d1d5b74ce9334747b0c6df47512c3b8
Nick Ryzhkov <namezys@getpatch.com>
2018-05-01 12:24:16 +0300

    save password to account

--- hornet/management/commands/hornet_account.py
+++ hornet/management/commands/hornet_account.py
@@ -32,20 +32,26 @@ class Command(BaseCommand):
         else:
             raise AssertionError("Unknown action")
 
-    def create(self, account, username, token, **kwargs):
+    def create(self, account, username, token, password, **kwargs):
         if account:
             self.stderr.write("Account exists")
             return
-        account = Account.objects.create(username=username, token=token)
+        account = Account.objects.create(username=username, token=token, password=password)
         self.stdout.write("Account created")
         self.print(account)
 
-    def update(self, account, token, **kwargs):
+    def update(self, account, token, password, **kwargs):
         if not account:
             self.stderr.write("Not found")
             return
-        account.token = token
-        account.save(update_fields=["token"])
+        update_fields = []
+        if token is not None:
+            account.token = token
+            update_fields += ["token"]
+        if password is not None:
+            account.password = password
+            update_fields += ["password"]
+        account.save(update_fields=update_fields)
         self.print(account)
 
     def print(self, account):
